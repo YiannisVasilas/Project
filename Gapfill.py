8b6e1192fd06a6cde79a48bda9b1cd711b7559b0
YiannisVasilas <43544024+YiannisVasilas@users.noreply.github.com>
2019-03-15 13:09:41 +0100

    Update Gapfill.py

--- Gapfill.py
+++ Gapfill.py
@@ -2,15 +2,14 @@ import cobra as cobra
 import re
 import json
 
-def make_MNXRef_ns_dict(ns,reverse=False):
+def make_something_ns_dict(ns,reverse=False):
     ### PURPOSE
-    # Creates a dictionary containing identifiers of namespace ns as keys and MNXRef identifiers as values
+    # Creates a dictionary containing identifiers of namespace ns as keys and select your identifiers as values
     ### EXAMPLE CALL
-    #   name_dict = make_MNXRef_ns_dict('metacyc',reverse=False)
+    #   name_dict = make_something_ns_dict('metacyc',reverse=False)
     ### INPUT
     #   ns: reference namespace. Current options are: metacyc, bigg, kegg, chebi, seed, upa, biopath, lipidmaps, hmdb, reactome, umbbd
-    ### OPTIONAL INPUT
-    #   reverse: If the dictionary should be swapped - MNXref ids as keys and ns ids as values
+    
     ### OUTPUT
     #   name_dict - mapping dictionary
     
@@ -33,40 +32,11 @@ def make_MNXRef_ns_dict(ns,reverse=False):
     
     #some manual additions / corrections:
     if ns=='bigg':
-        name_dict['hbut4coa'] = 'MNXM2334'
-        name_dict['mylcoa'] = 'MNXM1611'
-        name_dict['mcnlcoa'] = 'MNXM2693'
-        name_dict['mmylcoa'] = 'MNXM5993'
-        name_dict['hbut4'] = 'MNXM514'
-        name_dict['citmcoa'] = 'MNXM2358'
-        name_dict['3hpcoa'] = 'MNXM1263'
-        name_dict['2h3opp'] = 'MNXM475'
-        name_dict['CPD1065'] = 'MNXM5823'
-        name_dict['fdxo_4_2'] = 'MNXM145541'
-        name_dict['fdxr_4_2'] = 'MNXM145542'
-        name_dict['fdxr_2_2'] = 'MNXM169'
-        name_dict['f6p'] = 'MNXM162235'
-        name_dict['3oodcoa_c'] = 'MNXM513'
-        name_dict['4cmcoa_c'] = 'MNXM264'
-        name_dict['oxoarg__L'] = 'MNXM1037'
-        name_dict['chpd_c'] = 'MNXM9829'
-        name_dict['4h2oxov_c'] = 'MNXM1176'
-        name_dict['5mthglu_c'] = ''
-        name_dict['thglu_c'] = ''
-        name_dict['quin_kt_c'] = ''
-        name_dict['4iz5pp_c'] = ''
-        name_dict['urocan_c'] = ''
-        name_dict['slcys_c'] = ''
-        name_dict['codscl2_c'] = ''
-        name_dict['colipaA_c'] = ''
-        name_dict['glc_bD_c'] = ''
-        name_dict['glc_B_c'] = ''
-        name_dict['bcarote_c'] = ''
-        name_dict['atocophe_c'] = ''
-        name_dict['btocophe_c'] = ''
+        name_dict['something'] = 'something'
+      
 
     elif ns == 'seed':
-        name_dict['cpd00027'] = 'MNXM41' 
+        name_dict['something'] = 'something' 
 
     #Check if empty
     if len(name_dict)==0:
@@ -74,18 +44,18 @@ def make_MNXRef_ns_dict(ns,reverse=False):
     
     return name_dict
 
-def Translate_to_MNXRef(model,ns):
+def Translate_to_somethingf(model,ns):
     ### PURPOSE
     # Converts a model into the MNXref namespace
     ### EXAMPLE CALL
-    #   Translate_to_MNXRef(model,'metacyc')
+    #   Translate_to_(model,'metacyc')
     ### INPUT
     #   model: CobraPy model structure
     #   ns: Current namespace in model
     ### OPTIONAL INPUT
     ### OUTPUT
     
-    name_dict = make_MNXRef_ns_dict(ns)
+    name_dict = make_something_ns_dict(ns)
     if len(name_dict)==0:
         print 'Could not map: unknown namespace'
         return
@@ -109,13 +79,13 @@ def Translate_to_MNXRef(model,ns):
                 replace_delete(model, m, model.metabolites.get_by_id(newid))
         
     #then replace compound name, formula and charge
-    MNXRef_dict = MNXRef_met_props() #Gather metabolite properties
+    MNXRef_dict = something_met_props() #Gather metabolite properties
     for m in model.metabolites:
         mid = m.id[:-2]
-        if mid in MNXRef_dict:
-            m.name = MNXRef_dict[mid]['name']
-            m.formula = MNXRef_dict[mid]['formula']
-            m.charge = MNXRef_dict[mid]['charge']
+        if mid in something_dict:
+            m.name = somethingf_dict[mid]['name']
+            m.formula = something_dict[mid]['formula']
+            m.charge = something_dict[mid]['charge']
     model.repair()
     
     print '%d unmapped metabolites remain' % (len([m for m in model.metabolites if m.id[0:4]!='MNXM']))
@@ -141,13 +111,8 @@ def MNXRef_met_props():
                                              'formula':elems[2],
                                              'charge':int(elems[3])}
     
-    #some manual corrections:
-    MNXRef_dict['MNXM318']['charge'] = 0
-    MNXRef_dict['MNXM33']['charge'] = -2
-    MNXRef_dict['MNXM89582']['formula'] = 'H2S' 
-    MNXRef_dict['MNXM89582']['charge'] = 0
-
-    return MNXRef_dict
+ 
+    
 
 def replace_delete(model, old_met, new_met):
     ### PURPOSE
@@ -235,12 +200,7 @@ def mnxkeggdict():
                 mnxkegg[line[tablocs[0]+1:tablocs[1]]] = line[5:tablocs[0]]
         if line[0:10] == 'lipidmaps:':
             break
-    #some manual additions:
-    mnxkegg.update({'MNXM84':'C00014', 'MNXM191': 'C00390',  'MNXM232': 'C00399', 'MNXM145542': 'C00138', 'MNXM145541':'C00139',  'MNXM169': 'C00138', 'MNXM178':'C00139', 
-    'MNXM145809':'C02869', 'MNXM6271':'C02745', 'MNXM148165':'C06021', 'MNXM148349':'C06020', 'MNXM558':'C00390', 'MNXM2178': 'C00399', 'MNXM509': 'C00828',  'MNXM223':'C05819', 
-    'MNXM116':'C00117', 'MNXM15900':'C00117', 'MNXM162':'C00311','MNXM1783':'C00399',  'MNXM5319':'C00390', 'MNXM114366':'C14180','MNXM89629':'C00085','MNXM2378':'C00118','MNXM145523':'C00051','MNXM91032':'C18026',}) #Why 2? - '2 C00138', 'MNXM145541': '2 C00139',  'MNXM169': '2 C00138', 'MNXM178':'2 C00139'
-    xref.close()
-    return mnxkegg
+   
 
 def getkeggformationenergies(filename):
     # load formation energies from the JSON file
@@ -289,8 +249,7 @@ def check_samereaction(reactiona,  reactionb):
     return same#,  reverse
 
 def silence_oxygen_sensitive(model):
-    knockoutlist = ['POR5', 'POR_2', 'PFL', 'CODH_ACS', 'FDH7', 'RNF', 'OOR2', 'MNXR83963', 'MNXR4175', 
-    'MNXR82027', 'MNXR82814', 'MNXR83529', 'MNXR83965', 'MNXR35431', 'MNXR54890', 'MNXR85543']
+    knockoutlist = []
     for r in knockoutlist:
         if r in model.reactions:
             model.reactions.get_by_id(r).lower_bound = 0
