cb1849938188049819cb97223cbb3afffa1b270f
YiannisVasilas <43544024+YiannisVasilas@users.noreply.github.com>
2020-11-19 09:40:28 +0100

    Add files via upload

--- fastq_splitter.py
+++ fastq_splitter.py
@@ -1,7 +1,15 @@
 #!usr/bin/env python3
 """
-Author: Joeri, Marieke
+Authors: Joeri, Marieke
 
+script that splits fastq.gz file with concatenated reads to 2 files
+input: fastq.gz file
+output two fastq.gz files for left and right reads
+
+Functions:
+    unzipper: unzips .gz file
+    fastq_splitter: splits concatenated fastq file
+    zipper: zips fastq file to fastq.gz
 """
 # import statements
 from sys import argv
@@ -21,22 +29,57 @@ def unzipper(in_fn):
     """
     print("unzipping file...")
     out_fn = in_fn[:-3]
-    if not os.path.exists(in_fn):
-        raise ValueError("{} does not exist".format(in_fn))
+    
     if os.path.exists(out_fn):
-        print('... tool not run, {} already exsists.'.format(out_fn))
+        print('unzipping tool not run, {} already exists.'.format(out_fn))
         return out_fn
     cmd = 'gunzip {}'.format(in_fn)
     subprocess.check_call(cmd, shell = True)
     return out_fn
 
-def fastq_splitter(input_fn):
-    out_fn_1eft = input_fn[:-6] + '_left.fastq'
+def fastq_splitter(input_fn, read_length = 100):
+    """ splits fastq file with concatenated reads to left and right file
+    
+    Keyword arguments:
+        input_fn: string, name of input fastq file
+        read_length: integer, length of one part of the paired read
+    Returns:
+        tuple with names of both output files.
+    """
+    out_fn_left = input_fn[:-6] + '_left.fastq'
     out_fn_right = input_fn[:-6] + '_right.fastq'
-    with open(input_fn, 'r') as file_object:
+    
+    with open(input_fn, 'r') as input_file,\
+         open(out_fn_left, 'w') as left_file,\
+         open(out_fn_right, 'w') as right_file:
+             for line in input_file:
+                 if line.startswith('@') or line.startswith('+'):
+                     left_file.write(line)
+                     right_file.write(line)
+                 else:
+                     left_file.write(line[:read_length]+'\n')
+                     right_file.write(line[-read_length-1:])
+    return (out_fn_left, out_fn_right)
         
-        
-        
+def zipper(in_fn):
+    """
+    runs gzip tool, zips fastq.gz file  
+      
+    Keyword arguments:
+        in_fn: string, name of file to be zipped
+    Returns:
+        string, filename of zipped file
+    """
+    print("zipping file...")
+    out_fn = in_fn + '.gz'
+    if not os.path.exists(in_fn):
+        raise ValueError("{} does not exist".format(in_fn))
+    if os.path.exists(out_fn):
+        print('zipping tool not run, {} already exsists.'.format(out_fn))
+        return out_fn
+    cmd = 'gzip {}'.format(in_fn)
+    subprocess.check_call(cmd, shell = True)
+    return out_fn
     
 
 if __name__ == "__main__":
@@ -44,10 +87,15 @@ if __name__ == "__main__":
     filename = argv[1]
     
     # unzip file
-    unzipped_filename = unzipper(filename)
-    print(unzipped_filename)
+    #unzipped_filename = unzipper(filename)
+    #print(unzipped_filename)
     
     # parse and split fastq into two files
-    fastq_splitter(unzipped_filename)
+    left_out, right_out = fastq_splitter(argv[1])
     
+    # zip splitted files
+    zipped_left_out = zipper(left_out)
+    zipped_right_out = zipper(right_out)
     
+    print(zipped_left_out)
+    print(zipped_right_out)
